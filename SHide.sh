eb36ce559f010cb23316857dff86684456aa7ea9
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2019-09-05 14:05:34 +0530

    Add files via upload

--- SHide.sh
+++ SHide.sh
@@ -29,9 +29,54 @@ if [ $i -eq 1 ]
 
 then
 	
-	read -p "Enter file to hide :" sf
-	read -p "Enter file to hide within :" hf
-	steghide embed -ef $sf -cf $hf -Z
+	echo -n " 1 for DEFAULT SETTINGS and 2 for CUSTOM :"
+	read d
+
+	if [ $d -eq 1 ]
+
+	then
+
+		read -p "Enter file to hide :" sf
+		read -p "Enter file to hide within :" hf
+		steghide embed -ef $sf -cf $hf -Z
+
+	fi
+
+	if [ $d -eq 2 ]
+
+	then
+
+		echo "Do you want to compress before embeding data?"
+		echo -n " Press 1 to COMPRESS and 2 for NO COMPRESSION : "
+		read c 
+		
+		if [ $c -eq 1 ]
+
+		then
+
+			read -p "Enter file to hide :" sf
+			read -p "Enter file to hide within :" hf
+			read -p "Enter compression level 1-9. Where 1->best speed 9->best compression" cl
+			read -p "Enter a passphrase (whitespaces are accepted :" pp 
+
+			steghide embed -ef $sf -cf $hf --compress $cl -Z -p "$pp"
+
+		fi
+
+		if [ $c -eq 2 ]
+
+		then
+
+			read -p "Enter file to hide :" sf
+			read -p "Enter file to hide within :" hf
+			read -p "Enter a passphrase (whitespaces are accepted :" pp
+			steghide embed -ef $sf -cf $hf -Z -p "$pp"
+
+		fi
+
+
+
+	fi
 
 fi
 
